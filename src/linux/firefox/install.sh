e240340c62b671092df58397fd458aa9885338ef
Pawel Koszalka <koszalix@gmail.com>
2022-09-06 14:50:56 +0200

    fixed installer for linux/firefox

--- src/linux/firefox/install.sh
+++ src/linux/firefox/install.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 mkdir -p ~/.mozilla/native-messaging-hosts/
-sed -i -r 's+"path": "singlefile_companion_lite"+"path" :"'"$PWD"'\/singlefile_companion_lite"+' singlefile_companion.json
+mkdir -p ~/.singlefile/WebArchives
+
+sed -i 's@singlefile_companion_lite@'/home/$USER'/.singlefile/singlefile_companion_lite@' singlefile_companion.json 
 cp singlefile_companion.json ~/.mozilla/native-messaging-hosts/singlefile_companion.json
+
+cp singlefile_companion_lite ~/.singlefile/singlefile_companion_lite
+cp options.json ~/.singlefile/options.json
+
+chmod 755 ~/.singlefile/singlefile_companion_lite
+
+
