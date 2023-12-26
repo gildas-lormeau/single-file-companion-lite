7c6385eb77478f445edb238dde040692191f0fa8
Gildas <gildas.lormeau@gmail.com>
2023-12-26 16:14:18 +0100

    fix scripts

--- src/mac/chromium/install.sh
+++ src/mac/chromium/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/
-sed -i -r 's+"path": "singlefile_companion_lite"+"path" :"'"$PWD"'\/singlefile_companion_lite"+' singlefile_companion.json
+sed -i '' -r 's+"path": "singlefile_companion_lite"+"path" :"'"$PWD"'\/singlefile_companion_lite"+' singlefile_companion.json
 cp singlefile_companion.json ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/singlefile_companion.json
